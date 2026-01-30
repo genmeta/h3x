6127203738ff8cb5dd8e4b601f605a67edfc7249
eareimu <reimu@genmeta.net>
2026-01-30 18:11:30 +0800

    refactor: move unit tests to integration tests
    - Move simpl_server test from src/gm_quic/quic.rs to tests/simple.rs
    - Extract test utilities to tests/common/

---
+++ tests/common/mod.rs
@@ -0,0 +1,134 @@
+#![allow(unused)]
+
+use std::{
+    error::Error,
+    sync::{Arc, LazyLock},
+    time::Duration,
+};
+
+use gm_quic::{
+    prelude::{
+        BindUri, QuicIO, RealAddr,
+        handy::{ToCertificate, ToPrivateKey},
+    },
+    qinterface::component::route::QuicRouter,
+};
+use h3x::{
+    gm_quic::{H3Client, H3Servers},
+    server::UnresolvedRequest,
+};
+use http::uri::Authority;
+use tokio::time;
+use tracing::{Instrument, level_filters::LevelFilter};
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{
+    Layer, prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt,
+};
+
+pub fn run<F: Future>(test_name: &'static str, future: F) -> F::Output {
+    static RT: LazyLock<tokio::runtime::Runtime> = LazyLock::new(|| {
+        tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+    });
+
+    static TRACING: LazyLock<WorkerGuard> = LazyLock::new(|| {
+        let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
+
+        tracing_subscriber::registry()
+            // .with(console_subscriber::spawn())
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .with_writer(non_blocking)
+                    .with_file(true)
+                    .with_line_number(true)
+                    .with_filter(LevelFilter::DEBUG),
+            )
+            .with(tracing_subscriber::filter::filter_fn(|metadata| {
+                !metadata.target().contains("netlink_packet_route")
+            }))
+            .init();
+        guard
+    });
+
+    RT.block_on(async move {
+        LazyLock::force(&TRACING);
+        let test = future.instrument(tracing::info_span!("test", test_name));
+        match time::timeout(Duration::from_secs(60), test).await {
+            Ok(output) => output,
+            Err(_timedout) => panic!("test timed out"),
+        }
+    })
+}
+
+pub const CA_CERT: &[u8] = include_bytes!("../../tests/keychain/localhost/ca.cert");
+pub const SERVER_CERT: &[u8] = include_bytes!("../../tests/keychain/localhost/server.cert");
+pub const SERVER_KEY: &[u8] = include_bytes!("../../tests/keychain/localhost/server.key");
+pub const TEST_DATA: &[u8] = include_bytes!("mod.rs");
+
+pub fn test_client() -> H3Client {
+    let mut roots = rustls::RootCertStore::empty();
+    roots.add_parsable_certificates(CA_CERT.to_certificate());
+    H3Client::builder()
+        .with_root_certificates(roots)
+        .without_identity()
+        .expect("Failed to initialize client tls")
+        .with_router(Arc::new(QuicRouter::new()))
+        .build()
+}
+
+pub async fn test_server<S>(router: S) -> H3Servers<S>
+where
+    S: tower_service::Service<UnresolvedRequest, Response = ()> + Clone + Send + Sync + 'static,
+    S::Future: Send,
+    S::Error: Into<Box<dyn Error + Send + Sync>>,
+{
+    let mut servers = H3Servers::builder()
+        .without_client_cert_verifier()
+        .expect("Failed to initialize server tls")
+        .with_router(Arc::new(QuicRouter::new()))
+        .listen()
+        .expect("Failed to listen");
+    servers
+        .add_server(
+            "localhost",
+            SERVER_CERT.to_certificate(),
+            SERVER_KEY.to_private_key(),
+            None,
+            [
+                BindUri::from("inet://127.0.0.1:0").alloc_port(),
+                BindUri::from("inet://[::1]:0").alloc_port(),
+            ],
+            router,
+        )
+        .await
+        .expect("Failed to add server");
+    servers
+}
+
+pub fn get_server_addr<S>(servers: &H3Servers<S>) -> RealAddr {
+    let localhost = servers
+        .quic_listener()
+        .get_server("localhost")
+        .expect("Server localhost must be registered");
+    let (_bind_uri, localhost_bind_interface) = localhost
+        .bind_interfaces()
+        .into_iter()
+        .next()
+        .expect("Server localhost must have at least one bind interface");
+    localhost_bind_interface
+        .borrow()
+        .real_addr()
+        .expect("Bind interface must have local addr")
+}
+
+pub fn get_server_authority<S>(servers: &H3Servers<S>) -> Authority {
+    match get_server_addr(servers) {
+        RealAddr::Internet(socket_addr) => {
+            Authority::from_maybe_shared(Vec::from(format!("localhost:{}", socket_addr.port())))
+                .expect("Failed to parse authority")
+        }
+        _ => unimplemented!("Only Internet addresses are supported now"),
+    }
+}
---
+++ tests/simple.rs
@@ -0,0 +1,97 @@
+mod common;
+use common::run;
+use h3x::server::{self, Router};
+use tokio_util::task::AbortOnDropHandle;
+
+use crate::common::*;
+
+async fn hello_world_service(_: &mut server::Request, response: &mut server::Response) {
+    response
+        .set_status(http::StatusCode::OK)
+        .set_body(&b"Hello, World!"[..]);
+}
+
+#[test]
+fn hello_world() {
+    run("hello_world", async move {
+        let server = test_server(Router::new().get("/hello_world", hello_world_service)).await;
+        let host = get_server_authority(&server);
+        let _serve = AbortOnDropHandle::new(tokio::spawn(async move { server.run().await }));
+
+        let client = test_client();
+        let (_, mut response) = client
+            .new_request()
+            .get(format!("https://{host}/hello_world").parse().unwrap())
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(response.status(), http::StatusCode::OK);
+        let response = response
+            .read_to_string()
+            .await
+            .expect("Failed to read response body");
+        assert_eq!(response, "Hello, World!");
+    })
+}
+
+async fn echo_service(request: &mut server::Request, response: &mut server::Response) {
+    response.set_status(http::StatusCode::OK);
+    while let Some(chunk) = request
+        .read()
+        .await
+        .transpose()
+        .expect("Failed to read request body")
+    {
+        response
+            .write(chunk)
+            .await
+            .expect("Failed to write response body");
+    }
+}
+
+#[test]
+fn echo() {
+    run("echo", async move {
+        let server = test_server(Router::new().post("/echo", echo_service)).await;
+        let host = get_server_authority(&server);
+        let _serve = AbortOnDropHandle::new(tokio::spawn(async move { server.run().await }));
+
+        let client = test_client();
+        let (_, mut response) = client
+            .new_request()
+            .with_body(TEST_DATA)
+            .post(format!("https://{host}/echo").parse().unwrap())
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(response.status(), http::StatusCode::OK);
+        let response = response
+            .read_to_bytes()
+            .await
+            .expect("Failed to read response body");
+        assert_eq!(response, TEST_DATA);
+    })
+}
+
+#[test]
+fn fallback() {
+    run("fallback", async move {
+        let server = test_server(
+            Router::new()
+                .get("/hello_world", hello_world_service)
+                .post("/hello_world", hello_world_service),
+        )
+        .await;
+        let host = get_server_authority(&server);
+        let _serve = AbortOnDropHandle::new(tokio::spawn(async move { server.run().await }));
+
+        let client = test_client();
+        let (_, response) = client
+            .new_request()
+            .get(format!("https://{host}/non_exist").parse().unwrap())
+            .await
+            .expect("Failed to send request");
+
+        assert_eq!(response.status(), http::StatusCode::NOT_FOUND);
+    })
+}
