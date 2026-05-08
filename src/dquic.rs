026d691795861f5b1d4f441bf2fa5d48e2dc61bf
eareimu <reimu@genmeta.net>
2026-05-08 20:51:20 +0800

    refactor(h3x): remove redundant #[cfg(feature = "dquic")] from dquic.rs

--- src/dquic.rs
+++ src/dquic.rs
@@ -1,26 +1,16 @@
-#[cfg(feature = "dquic")]
 mod endpoint;
-#[cfg(feature = "dquic")]
 pub mod network;
-#[cfg(feature = "dquic")]
 pub mod identity;
-#[cfg(feature = "dquic")]
 pub mod sni;
-#[cfg(feature = "dquic")]
 pub mod quic_config;
-#[cfg(feature = "dquic")]
 pub mod binds;
 
 mod shim;
 
 pub use dquic::*;
-#[cfg(feature = "dquic")]
 pub use endpoint::*;
-#[cfg(feature = "dquic")]
 pub use identity::*;
-#[cfg(feature = "dquic")]
 pub use network::*;
-#[cfg(feature = "dquic")]
 pub use quic_config::*;
 
 use std::error::Error;
@@ -30,10 +20,8 @@ use tower_service::Service;
 use crate::endpoint::server::UnresolvedRequest;
 
 /// Type alias for the default `H3Endpoint<QuicEndpoint>`.
-#[cfg(feature = "dquic")]
 pub type H3Endpoint = crate::endpoint::H3Endpoint<QuicEndpoint>;
 
-#[cfg(feature = "dquic")]
 impl crate::endpoint::H3Endpoint<QuicEndpoint> {
     /// Serve HTTP/3 requests on `&self`, constructing a temporary
     /// `H3Endpoint<&QuicEndpoint>` internally to drive the generic
